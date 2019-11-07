5ef0c496ee47611e28b5a61afc54e3a79648aace
Sole Selusa <sselusa@oa-m2.21-school.ru>
2019-11-07 21:50:02 +0300

    Add output part

---
+++ sources/output/ft_putstr_fd.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sselusa <sselusa@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/07 21:44:40 by sselusa           #+#    #+#             */
+/*   Updated: 2019/11/07 21:45:05 by sselusa          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_libft.h"
+
+void				ft_putstr_fd(const char *s, int fd)
+{
+	size_t	len;
+	size_t	symbols;
+
+	if (!s)
+		return ;
+	len = ft_strlen(s);
+	symbols = 0;
+	while (symbols < len)
+		ft_putchar_fd(s[symbols++], fd);
+}
