3bc885815c2cdfa42560f8f1494d8143ccc0650f
Sole Selusa <sselusa@oa-m2.21-school.ru>
2019-11-07 21:43:00 +0300

    Add xtoy part

---
+++ sources/xtoy/ft_atoi.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sselusa <sselusa@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/07 21:36:29 by sselusa           #+#    #+#             */
+/*   Updated: 2019/11/07 21:42:24 by sselusa          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_libft.h"
+
+static size_t		ft_pass_spaces(const char *str)
+{
+	size_t			pos;
+
+	pos = 0;
+	if (!str)
+		return (pos);
+	while ((str[pos] > 8 && str[pos] < 14) || str[pos] == 32)
+		pos++;
+	return (pos);
+}
+
+static size_t		ft_get_sign(const char *str, size_t *pos)
+{
+	int				sign;
+
+	sign = 1;
+	if (str[*pos] == '-' || str[*pos] == '+')
+	{
+		if (str[*pos] == '-')
+			sign = -1;
+		(*pos)++;
+	}
+	return (sign);
+}
+
+int					ft_atoi(const char *str)
+{
+	unsigned long		num;
+	int					sign;
+	size_t				pos;
+
+	num = 0;
+	pos = pass_spaces(str);
+	sign = get_sign(str, &pos);
+	while (ft_isdigit(str[pos]))
+	{
+		if (sign == 1 && (num > (UL_MAX / 10) ||
+			(num == (UL_MAX / 10) && str[pos] - '0' > 7)))
+			return (-1);
+		if (sign == -1 && (num > (UL_MAX / 10) ||
+			(num == (UL_MAX / 10) && str[pos] - '0' > 8)))
+			return (0);
+		num = num * 10 + str[pos] - '0';
+		pos++;
+	}
+	return ((int)(sign * num));
+}
---
+++ sources/xtoy/ft_itoa.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sselusa <sselusa@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/07 21:33:18 by sselusa           #+#    #+#             */
+/*   Updated: 2019/11/07 21:36:17 by sselusa          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_libft.h"
+
+char				*ft_itoa(int n)
+{
+	unsigned int		num;
+	int					symbols;
+	char				*str;
+	int					is_negative;
+
+	is_negative = n < 0 ? 1 : 0;
+	num = n * (is_negative ? -1 : 1);
+	symbols = 1 + is_negative;
+	while (n / 10)
+	{
+		n = n / 10;
+		symbols++;
+	}
+	if (!(str = ft_strnew(symbols)))
+		return (NULL);
+	if (is_negative)
+		str[0] = '-';
+	while (symbols-- >= 1 + is_negative)
+	{
+		str[symbols] = num % 10 + '0';
+		num = num / 10;
+	}
+	return (str);
+}
